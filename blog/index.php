eefc774aeb25c8f9842af2d51111d8da68852007
Natrajan Jaiswal <njaiswal78@gmail.com>
2018-01-04 23:52:01 +0530

    live jaguar

---
+++ blog/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+<title>Jaguar Blog</title>
+<link rel="stylesheet" type="text/css" href="blog.css">
+</head>
+<body>
+<div id="over">
+ <img src="back.jpg">
+</div>
+<div id="content">
+<h1 style="text-align:center;margin-top:250px;">Smart Investing: Buy low & Sell high </h1>
+<ul style="text-align:justify;">
+<li>
+Stock are issued in public interest by listed companis to raise public money for growth of their organization. Once a company is listed in stock exchange, an individual can buy it's stock and earn profits if stock prices rise. First step to enter into market is to open your Demat acount and trading account with stock brokers.
+</li>
+<br>
+<li>
+Once you have trading platform at your finguretips, DO NOT INVEST IN EQUITIES without understanding market dynamics. In 99% scenario, most of you will lose your moeny. Even fundamental analysis, technical analysis of stock or balance sheet, income statement of companies are not going to help if you plan to are intraday or swing trader. Above analysis strengthens probability of judgement for long players.  
+</li>
+<br>
+<li>
+Market is sensitivity to news but in most cases, people have lost money on reacting to market news. This is because of inner trading which is illigal and because of this you miss the right moment and market goes somewhere else from your judgement. NEVER INVEST ON THIRD PARTY TIPS, many people fall in this trap and loose their hard earned money. If you want to be market player, come out and learn and invest accordingly.
+</li>
+</ul>
+</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<meta name="viewport" content="width=850">
+<title>Jaguar Profits</title>
+<link rel="icon" href="logo.jpg" type="image/gif" sizes="16x16">
+</head>
+<body style='background-color:black;'>
+<div align='center'>
+<div style='height:120px;background-color:white;margin-top:-10px;margin-left:-10px;margin-right:-10px;'>
+<img src='logo.jpg' alt='jaguar profits' style='width:150px;height:80px;'><span style='font-size:70px;'>Jaguar Profits</span></img>
+
+
+<script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
+<script type="IN/FollowCompany" data-id="13302212" data-counter="right"></script>
+<a href="http://jaguarprofits.blogspot.in/"><img src="blogicon.png" style="width:130px;height:100px;"></img></a>
+
+<br><br>
+<p style='color:white;'>Financial firm for helping in your needs of investing in mutual funds, equities <br>and teaching you how to trade smartly in Equity Markets.</p>
+</div>
+<br><br><br><br><br>
+
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRyjS9EIuZjGEiGQVYUOAZhj5wz3rijaC3gcxNwdKlFJBG0yqabjWorvNORTRszKsV6YskMXAqoP3_f/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" width="580" height="350"></iframe>
+
+<br><br>
+
+<a href='https://www.linkedin.com/company-beta/13302212/'><button style='width:70px;height:20px;border:2px;background-color:orange;cursor:pointer;'>Stocks</button></a>
+
+
+
+<br><br><br><br>
+
+<p style='color:white;'>Send your queries at admin@jaguarprofits.com</p>
+<p style='margin-top:-10px;color:white;'>+91-9653145275</p>
+</div>
+</body>
+
+</html>
