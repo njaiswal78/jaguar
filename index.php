2f5b97fe764c9dda24e797bcf2c21362b3f9e0f0
Natrajan Jaiswal <njaiswal78@gmail.com>
2018-01-13 17:09:08 +0530

    fb plugin addition

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 <link rel="icon" href="logo.jpg" type="image/gif" sizes="16x16">
 </head>
 <body style='background-color:black;'>
+
+<!-- Facebook SDK -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.11';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Facebook SDK -->
+
+
+
 <div align='center'>
 <div style='height:120px;background-color:white;margin-top:-10px;margin-left:-10px;margin-right:-10px;'>
 <img src='logo.jpg' alt='jaguar profits' style='width:150px;height:80px;'><span style='font-size:70px;'>Jaguar Profits</span></img>
@@ -18,11 +32,17 @@
 <p style='color:white;'>Financial firm for helping in your needs of investing in mutual funds, equities <br>and teaching you how to trade smartly in Equity Markets.</p>
 </div>
 <br><br><br><br><br>
-<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRyjS9EIuZjGEiGQVYUOAZhj5wz3rijaC3gcxNwdKlFJBG0yqabjWorvNORTRszKsV6YskMXAqoP3_f/pubhtml?widget=true&amp;headers=false"></iframe>
+
+<div id="box">
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRyjS9EIuZjGEiGQVYUOAZhj5wz3rijaC3gcxNwdKlFJBG0yqabjWorvNORTRszKsV6YskMXAqoP3_f/pubhtml?widget=true&amp;headers=false" width="620" height="350"></iframe>
+
+<div class="fb-page" data-href="https://www.facebook.com/Jaguar-Profits-493547391046016/" data-tabs="timeline,messages" data-width="400"
+ data-height="350" data-small-header="true" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="false"><blockquote cite="https://www.facebook.com/Jaguar-Profits-493547391046016/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Jaguar-Profits-493547391046016/">Jaguar Profits</a></blockquote></div>
+
+</div>
 
 <br><br>
-
-<a href='https://www.linkedin.com/company-beta/13302212/'><button style='width:70px;height:20px;border:2px;background-color:orange;cursor:pointer;'>Stocks</button></a>
+<a href='https://www.linkedin.com/company-beta/13302212/'><button style='width:230px;height:20px;border:2px;background-color:orange;cursor:pointer;'>Stocks Recommendations on Linkedin</button></a>
 
 
 
